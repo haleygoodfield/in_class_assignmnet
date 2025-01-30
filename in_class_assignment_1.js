6b7e80ce299ee29959399f45a3a28a081b7bf5e1
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-30 16:18:12 -0500

    Task #1 and Task #2 Completion

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,15 @@
-// Task 1
+// Task 1 
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log("Manageable Expenses");
+}
 
+// Task 2 
+let score = 75;
+if (score >= 60) {
+     console.log("Passed");
+} else {
+    console.log("Failed");
+}
